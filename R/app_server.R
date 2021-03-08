8f25bf0ea1089b67275b3c70a34ca55e6da251f7
Lionel Voirol <lionelvoirol@hotmail.com>
2021-03-08 21:13:19 +0100

    update server for cleaner code

--- R/app_server.R
+++ R/app_server.R
@@ -7,9 +7,10 @@
 app_server <- function( input, output, session ) {
   # List the first level callModules here
   
-
-  output$summary <- renderText({
+  # create sh script
+  sh_script = reactive({
     
+    # time
     time_hour_min = paste(sprintf('%02d', input$time_hour), sprintf('%02d', input$time_min), "00" , sep= ":")
     time = paste(input$time_day, time_hour_min, sep = "-")
     
@@ -20,15 +21,12 @@ app_server <- function( input, output, session ) {
     all_mail_type =  paste(input$mail_type_2, collapse = ",")
     
     # load command
-    load_cmd = paste("module load GCC/8.3.0  OpenMPI/3.1.4 R/", input$r_version_2, sep = "")
-    
-    # load command
     if(input$r_version_2 == "3.6.0"){
-      load_cmd = paste("module load GCC/8.2.0-2.31.1  OpenMPI/3.1.3 R/", input$r_version_2, sep = "")
+      load_cmd = paste("module load GCC/8.2.0-2.31.1 OpenMPI/3.1.3 R/", input$r_version_2, sep = "")
     }else if(input$r_version_2 == "3.6.2"){
-      load_cmd = paste("module load GCC/8.3.0  OpenMPI/3.1.4 R/", input$r_version_2, sep = "")
+      load_cmd = paste("module load GCC/8.3.0 OpenMPI/3.1.4 R/", input$r_version_2, sep = "")
     }else if(input$r_version_2 == "4.0.0"){
-      load_cmd = paste("module load GCC/9.3.0  OpenMPI/4.0.3 R/", input$r_version_2, sep = "")    }  
+      load_cmd = paste("module load GCC/9.3.0 OpenMPI/4.0.3 R/", input$r_version_2, sep = "")    }  
     
     # print bash script
     paste("#!/bin/bash", 
@@ -43,6 +41,12 @@ app_server <- function( input, output, session ) {
           paste("srun R CMD BATCH ", input$file_name, input$report_name),
           sep = "\n")  
     
+  })
+  
+
+  output$summary <- renderText({
+    
+    sh_script()
     
   })
   
@@ -51,41 +55,8 @@ app_server <- function( input, output, session ) {
   # # Add clipboard buttons
   output$clip <- renderUI({
     
-    time_hour_min = paste(sprintf('%02d', input$time_hour), sprintf('%02d', input$time_min), "00" , sep= ":")
-    time = paste(input$time_day, time_hour_min, sep = "-")
+    rclipButton("clipbtn", "Copy ", clipText = sh_script(), icon("clipboard"))
     
-    # partitions
-    all_partitions_2 = paste(trimws(as.vector(input$partition_2)), collapse = ",")
-    
-    # mail type
-    all_mail_type =  paste(input$mail_type_2, collapse = ",")
-    
-    # load command
-    load_cmd = paste("module load GCC/8.3.0  OpenMPI/3.1.4 R/", input$r_version_2, sep = "")
-    
-    # load command
-    if(input$r_version_2 == "3.6.0"){
-      load_cmd = paste("module load GCC/8.2.0-2.31.1  OpenMPI/3.1.3 R/", input$r_version_2, sep = "")
-    }else if(input$r_version_2 == "3.6.2"){
-      load_cmd = paste("module load GCC/8.3.0  OpenMPI/3.1.4 R/", input$r_version_2, sep = "")
-    }else if(input$r_version_2 == "4.0.0"){
-      load_cmd = paste("module load GCC/9.3.0  OpenMPI/4.0.3 R/", input$r_version_2, sep = "")    }  
-    
-    # print bash script
-    sh_script = paste("#!/bin/bash", 
-          paste("#SBATCH --job-name=", input$jobname, sep = ""),
-          paste("#SBATCH --ntasks-per-node=", input$task_per_node, sep = ""),
-          paste("#SBATCH --cpus-per-task=", input$cpu_per_taks, sep = ""),
-          paste("#SBATCH --time=", time, sep = ""),
-          paste("#SBATCH --partition=", all_partitions_2, sep = ""),
-          paste("#SBATCH --mail-user=", input$mail_user, sep = ""),
-          paste("#SBATCH --mail-type=", all_mail_type, sep = ""),
-          load_cmd,
-          paste("srun R CMD BATCH ", input$file_name, input$report_name),
-          sep = "\n")  
-    
-    
-    rclipButton("clipbtn", "Copy ", clipText = sh_script, icon("clipboard"))
   })
   
 
